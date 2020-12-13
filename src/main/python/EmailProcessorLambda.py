6822269515b62d238acaf32a767ef4b44262dce1
spmunna <spmunna@gmail.com>
2020-12-12 19:35:47 -0500

    Update EmailProcessorLambda.py

--- src/main/python/EmailProcessorLambda.py
+++ src/main/python/EmailProcessorLambda.py
@@ -3,10 +3,11 @@ import email
 import os
 from botocore.exceptions import ClientError
 
-def lambda_handler(event, context):
+S3BucketName='sanjay-transcribe-new'
+DynamoRegion='us-east-1'
+DynamoTableName='Transcribe'
 
-	S3BucketName='bucket_name'
-	DynamoRegion='region'#us-east-1
+def lambda_handler(event, context):
 	
     transcribe_client = boto3.client('transcribe')
     
@@ -40,24 +41,10 @@ def lambda_handler(event, context):
                 s3.put_object(Body=part.get_payload(decode=True), Bucket=S3BucketName, Key=newKey)
    
                 put_item(fileName, fromAdd, newKey)
-                
-                #get_item(newKey)
-   
-                #s3.put_object_acl(Bucket="sanjay-transcribe", Key=fileName, ACL='public-read')
-                #url = s3.generate_presigned_url('get_object',
-                #                                    Params={'Bucket': 'sanjay-transcribe-new',
-                #                                            'Key': fileName},
-                #                                    ExpiresIn=120)
-                #                                    
-                #print("pre-signed URL : " + url)          
-                #url = "s3://sanjay-transcribe-new/2020-10-23T00_56_55.642Z.wav"
-                #transcribe_file('email-job', url, transcribe_client)
-    #parsed_msg = email.message_from_bytes(raw_msg['messageContent'].read())
-    #print(parsed_msg)
-
+ 
 def put_item(fileName, email, msgId):
     dynamodb = boto3.resource('dynamodb', region_name=DynamoRegion)
-    table = dynamodb.Table('Transcribes')
+    table = dynamodb.Table(DynamoTableName)
     response = table.put_item(
        Item={
             'FILE_NAME': fileName,
@@ -69,7 +56,7 @@ def put_item(fileName, email, msgId):
  
 def get_item(msgId):
     dynamodb = boto3.resource('dynamodb', region_name=DynamoRegion)
-    table = dynamodb.Table('Transcribes')
+    table = dynamodb.Table(DynamoTableName)
     try:
         response = table.get_item(Key={'MSG_ID': msgId})
     except ClientError as e:
@@ -102,3 +89,4 @@ def transcribe_file(job_name, file_uri, transcribe_client):
         else:
             print(f"Waiting for {job_name}. Current status is {job_status}.")
         time.sleep(10)    
+			
