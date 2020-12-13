d57653daa38b0dfe0841232e9bde89f7ae381b24
spmunna <spmunna@gmail.com>
2020-12-12 21:28:34 -0500

    Update Constants.java

--- src/main/java/transcribe/Constants.java
+++ src/main/java/transcribe/Constants.java
@@ -7,6 +7,6 @@ public final class Constants {
 	public static final Regions awsSeerviceRegion = Regions.US_EAST_1;
 	public static final String bucketName = "";
 	public static final String dynamoDBTable = "Transcribe";
-	public static final String senderEmail = "email_of_SES_account_sending_emails_back_to_users";
+	public static final String senderEmail = "a verified email in SES that will be used to send emails back to users";
 
 }
