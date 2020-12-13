7d1b5f3bc013d8974b469f8f23b31d99e96f05fe
spmunna <spmunna@gmail.com>
2020-12-13 11:30:46 -0500

    Update create-bucket.sh

--- create-bucket.sh
+++ create-bucket.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 BUCKET_ID=$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
-BUCKET_NAME=sanjay-artifacts-$BUCKET_ID
+BUCKET_NAME=transcribe-email
 echo $BUCKET_NAME > bucket-name.txt
 aws s3 mb s3://$BUCKET_NAME
