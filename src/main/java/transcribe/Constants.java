bb51ff709e411ed7c4afc07504db1ec2b5b585fc
spmunna <spmunna@gmail.com>
2020-12-12 19:38:37 -0500

    Update Constants.java

--- src/main/java/transcribe/Constants.java
+++ src/main/java/transcribe/Constants.java
@@ -1,11 +1,12 @@
 package transcribe;
 
+import com.amazonaws.regions.Regions;
+
 public final class Constants {
-
-	public static final String awsAccessKey = "";
-	public static final String awsSecretKey = "";
+	
+	public static final Regions awsSeerviceRegion = Regions.US_EAST_1;
 	public static final String bucketName = "";
-	public static final String senderEmail = "";
-
+	public static final String dynamoDBTable = "Transcribe";
+	public static final String senderEmail = "email_of_SES_account_sending_emails_back_to_users";
 
 }
